20979db13678756f7b043f5ffc07385fbd796cd0
Joseph Bolduc <josephjittin@gmail.com>
2024-11-21 17:04:56 -0600

    got rid of entity scale

--- Serialization/Serializer.cs
+++ Serialization/Serializer.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+using System.IO;
 using System.Text;
 using gHammerMapEditor.Types;
 
@@ -17,7 +17,7 @@ public static class Serializer
 		foreach (var brush in brushes)
 		{
 			if (brush is Brush b) sb.Append($"Brush: {b.GetTranslate} {b.GetRotation} {b.GetScale} {b.texture}\n");
-			else if (brush is Entity e) sb.Append($"Entity: {e.GetTranslate} {e.GetRotation} {e.GetScale} {e.Type switch { Enums.EntityType.OmniLight => "Omni", Enums.EntityType.DirectLight => "Spot", Enums.EntityType.Spawn => "Spawn", Enums.EntityType.Void => "Void", _ => throw new System.NotImplementedException() }}\n");
+			else if (brush is Entity e) sb.Append($"Entity: {e.GetTranslate} {e.GetRotation} {e.Type switch { Enums.EntityType.OmniLight => "Omni", Enums.EntityType.DirectLight => "Spot", Enums.EntityType.Spawn => "Spawn", Enums.EntityType.Void => "Void", _ => throw new System.NotImplementedException() }}\n");
 
 		}
 
